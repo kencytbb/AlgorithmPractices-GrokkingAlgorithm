f3ead5f7eaa791cdf9b1154ccbccd187e4698743
[Pioneer] Tai Nguyen <tai.nguyen01@axonactive.com>
2020-05-27 16:59:42 +0700

    Chapter1-BinarySearch Example

---
+++ BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+
+    public int search(int[] list, int item) {
+        int low = 0;
+        int high = list.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = list[mid];
+            if (guess == item) {
+                return mid;
+            }
+            if (guess > item) {
+                high = mid - 1;
+
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] list = new int[] { 1, 3, 5, 7, 9, 11, 13 };
+        BinarySearch binarySearch = new BinarySearch();
+        System.out.println("the value position is " + binarySearch.search(list, 11));
+    }
+}
