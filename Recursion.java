6bfa36ed4e34019c2568f366e1b6b63517d35a2f
unknown <taikency@gmail.com>
2020-05-30 14:37:07 +0700

    Add recrusive sum array

--- Recursion.java
+++ Recursion.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Recursion {
     
     public int fact(int x){
@@ -7,10 +9,21 @@ public class Recursion {
         return x * fact(x-1);
     }
 
+    public int DCStrategy(int[] arr){
+        if(arr.length <= 0){
+            return 0;
+        }
+       return  arr[0] + DCStrategy(Arrays.copyOfRange(arr, 1, arr.length));
+    }
+
     public static void main(String[] args) {
         Recursion recursive = new Recursion();
         int factNumber = recursive.fact(3);
         System.out.println(factNumber);
+
+        int[] arr = new int[]{2,3,5,7};
+        int total = recursive.DCStrategy(arr);
+        System.out.println(total);
 
     }
 }
