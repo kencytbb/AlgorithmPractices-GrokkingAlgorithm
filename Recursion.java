501f4caef4d08a12f90a01b29d22811ffc09470c
unknown <taikency@gmail.com>
2020-05-30 17:24:29 +0700

    add findMaximumNumber to find maximum number of an array

--- Recursion.java
+++ Recursion.java
@@ -23,6 +23,15 @@ public class Recursion {
          return 1 + countElementOfArray(Arrays.copyOfRange(arr, 1, arr.length));        
     }
 
+
+    public int findMaximumNumber(int[] arr){
+        if(arr.length <= 0){
+            return 0;
+        }
+        int nextNumber = findMaximumNumber(Arrays.copyOfRange(arr, 1, arr.length)) ;
+        int currentNumber = arr[0];
+        return  currentNumber > nextNumber ? currentNumber : nextNumber;
+    }
     public static void main(final String[] args) {
         final Recursion recursive = new Recursion();
         final int factNumber = recursive.fact(3);
@@ -33,13 +42,16 @@ public class Recursion {
         final int[] emptyArr = new int[] {};
         final int total = recursive.sumArray(arr);
         final int totalEmpty = recursive.sumArray(emptyArr);
-        System.out.println(total);
-        System.out.println(totalEmpty);
+        System.out.println("sum of all elements in array is: " + total);
+        System.out.println("sum of all elements in array empty is: " + totalEmpty);
 
         //count element in array - we can use array.length, but I'm using recursive to count total element in an array
-        final int[] countArr = new int[] { 2, 3, 5, 7, 10 };
+        final int[] countArr = new int[] { 2, 13, 5, 7, 20 };
         int count = recursive.countElementOfArray(countArr);
-        System.out.println(count);
+        System.out.println("count total elements in array is " + count); 
+
+        int maximumNumber = recursive.findMaximumNumber(countArr);
+        System.out.println("Maximum number is "+ maximumNumber);
 
     }
 }
